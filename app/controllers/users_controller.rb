94c1c201fcfc33c1f7947b792223e52f2ac2dcc8
Taisei19 <cherrychan0602@gmail.com>
2024-08-13 23:21:51 +0900

    0813-1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
+  
+  def create
+    @user = User.new(user_params)
+    if params[:user][:username] == ENV['TEACHER_USERNAME'] && params[:user][:password] == ENV['TEACHER_PASSWORD']
+      @user.role = "teacher"
+    else
+      @user.role = "student"
+    end
+  
+    if @user.save
+      # 成功時の処理
+    else
+      # エラー処理
+    end
+  end
+  
   def show
     @user = User.find(params[:id])
   end
